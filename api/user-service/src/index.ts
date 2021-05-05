c45ea41e1fea83f8258db26b5ee77f1fa87b3cde
BernardoRodrigues <bernardo.qtr.21@gmail.com>
2021-05-05 11:25:40 +0100

    added service for users

--- api/user-service/src/index.ts
+++ api/user-service/src/index.ts
@@ -6,10 +6,11 @@ import express from 'express'
 import logger from 'morgan'
 // import { version } from '../package.json'
 import { json } from 'body-parser'
-import * as httpCreateServer from 'http';
-import * as httpsCreateServer from 'https'
+import { createServer as httpCreateServer } from 'http';
+import { createServer as httpsCreateServer } from 'https'
 import userRouter from './routes/user-routes'
-const createServer = process.env.NODE_ENV === 'production' ? httpsCreateServer.createServer : httpCreateServer.createServer;
+
+const createServer = process.env.NODE_ENV === 'production' ? httpsCreateServer : httpCreateServer;
 const app = express();
 const version = `v${require('./../../package.json').version.split('.')[0]}`
 console.log(`Api version: ${version}`);
@@ -17,6 +18,7 @@ const port = process.env.PORT || 3000;
 const server = createServer(
     app.use(logger('dev'))
         .use(json())
+        .use(cors())
         .use(`/api/${version}/user`, userRouter)
         .use('*', (_, res) => res.status(404).json({message: 'Page not found'}))
     )
